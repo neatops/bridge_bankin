0b7deab1d79f6d8b68e13ad15220b28af71e1c64
Olivier Buffon <olivier@buffon.dev>
2020-12-16 15:48:24 -0500

    Init gem

---
+++ lib/bridge_bankin.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bridge_bankin/version"
+
+module BridgeBankin
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/bridge_bankin_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe BridgeBankin do
+  it "has a version number" do
+    expect(BridgeBankin::VERSION).not_to be nil
+  end
+end
