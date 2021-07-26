07daf423cd43345da764c305c2f0ebd5f960f9ad
Denis Notin <denis.notin@gmail.com>
2021-07-26 17:32:32 +0300

    Fixed failed tests and rubocop

--- lib/bridge_bankin/account.rb
+++ lib/bridge_bankin/account.rb
@@ -21,6 +21,7 @@ module BridgeBankin
       def list(access_token:, **params)
         protected_resource(access_token) do
           data = api_client.get("/v2/accounts", **params)
+          # binding.pry
           convert_to_bridge_object(**data)
         end
       end
