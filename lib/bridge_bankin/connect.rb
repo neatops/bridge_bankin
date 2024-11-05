68a2b8057057470b0d949cb98b53dbf399b89c79
Olivier Buffon <olivier@buffon.dev>
2024-11-05 09:32:13 -0500

    Update connect endpoints

--- lib/bridge_bankin/connect.rb
+++ lib/bridge_bankin/connect.rb
@@ -18,22 +18,7 @@ module BridgeBankin
       #
       def connect_item(access_token:, **params)
         protected_resource(access_token) do
-          data = api_client.get("/v2/connect/items/add/url", **params)
-          convert_to_bridge_object(**data)
-        end
-      end
-
-      #
-      # Returns the URL to Bridge's Connect funnel for adding an item using IBAN
-      #
-      # @param [String] access_token the access token provided during the user authentication
-      # @param [Hash] params any params that might be required (or optional) to communicate with the API
-      #
-      # @return [BridgeObject] the URL to Bridge's Connect funnel for adding an item using IBAN
-      #
-      def connect_item_with_iban(access_token:, **params)
-        protected_resource(access_token) do
-          data = api_client.post("/v2/connect/items/add/url", **params)
+          data = api_client.post("/v2/connect/items/add", **params)
           convert_to_bridge_object(**data)
         end
       end
@@ -48,7 +33,7 @@ module BridgeBankin
       #
       def edit_item(access_token:, **params)
         protected_resource(access_token) do
-          data = api_client.get("/v2/connect/items/edit/url", **params)
+          data = api_client.get("/v2/connect/items/edit", **params)
           convert_to_bridge_object(**data)
         end
       end
@@ -70,21 +55,6 @@ module BridgeBankin
       end
 
       #
-      # Request the URL to Bridge's Connect funnel for validating a user's email
-      #
-      # @param [String] access_token the access token provided during the user authentication
-      # @param [Hash] params any params that might be required (or optional) to communicate with the API
-      #
-      # @return [BridgeObject] the URL to Bridge's Connect funnel for validating a user's email
-      #
-      def validate_email(access_token:, **params)
-        protected_resource(access_token) do
-          data = api_client.get("/v2/connect/users/email/confirmation/url", **params)
-          convert_to_bridge_object(**data)
-        end
-      end
-
-      #
       # Request the URL to Bridge's Connect funnel for validating pro items
       #
       # @param [String] access_token the access token provided during the user authentication
@@ -94,7 +64,7 @@ module BridgeBankin
       #
       def validate_pro_items(access_token:, **params)
         protected_resource(access_token) do
-          data = api_client.get("/v2/connect/items/pro/confirmation/url", **params)
+          data = api_client.get("/v2/connect/items/pro/confirmation", **params)
           convert_to_bridge_object(**data)
         end
       end
